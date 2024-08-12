586dcc46d8985bb7f2003a13a91ff2f77c9734cd
Roberto <robertomdev@gmail.com>
2024-08-12 20:41:04 +0200

    * Modified index.tsx to remove .tsx file extension to .ts * Exporting parts of the library in index.tsx.

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import './styles.scss';
+
+export { LoggerProvider } from './logging-system/LoggerProvider';
+export { LoggerObserverScope, LoggerProducerScope } from './logging-system/LoggerContext';
+
+export { BannerComponent } from './components/banner-component/BannerComponent';
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'production',
-  entry: './src/index.tsx',
+  entry: './src/index.ts',
   output: {
     filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
